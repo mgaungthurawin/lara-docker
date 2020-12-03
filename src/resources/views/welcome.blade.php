8916e13a0d82e3878304bbb9357e86ea0bff626a
thura <aung.thura@securelinkmm.com>
2020-12-03 14:07:01 +0630

    second commit

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Start With Jenkins
                 </div>
 
                 <div class="links">
