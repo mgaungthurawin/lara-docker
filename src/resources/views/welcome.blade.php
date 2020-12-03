a60b494e21256d3956c8111e72bee866a5378797
thura <aung.thura@securelinkmm.com>
2020-12-03 14:16:51 +0630

    update welcome blade

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Start With Jenkins
+                    Start Jenkins
                 </div>
 
                 <div class="links">
