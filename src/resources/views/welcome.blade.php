0aa9eab0834bfbefaa878f4246d87738d8cbc5a0
thura <aung.thura@securelinkmm.com>
2020-12-03 14:34:14 +0630

    third commit

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Start Jenkins
+                    Jenkins
                 </div>
 
                 <div class="links">
