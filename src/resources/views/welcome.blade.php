10ba832fb91e25d462aa53e397a859c166de7c19
thura <aung.thura@securelinkmm.com>
2020-12-03 15:03:39 +0630

    forth commit

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Jenkins
+                    Local -> Git -> Jenkin -> Server
                 </div>
 
                 <div class="links">
